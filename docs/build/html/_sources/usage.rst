d5fcb9dc0a302e429dfe3383c5064f79e0ab9519
Pierre Boistier <pierre.boistier@cea.fr>
2024-04-26 16:45:17 +0200

    Advanced notebook on oscillation in vacuum

--- docs/build/html/_sources/usage.rst
+++ docs/build/html/_sources/usage.rst
@@ -1,13 +1,54 @@
 Usage
 =====
 
-.. _installation:
-
 Installation
 ------------
 
-To use Lumache, first install it using pip:
+To use this project on your machine, you can copy the repository:
+
+.. tabs::
+
+   .. tab:: GitHub-CLI
+
+      .. code-block:: console
+
+         gh repo clone pboistier/energy-effects-T2K
+   
+   .. tab:: SSH
+
+      .. code-block:: console
+
+         git clone git@github.com:pboistier/energy-effects-T2K.git
+
+   .. tab:: HTTPS
+
+      .. code-block:: console
+
+         git clone https://github.com/pboistier/energy-effects-T2K.git
+
+Then, you'll need pip and pdm:
 
 .. code-block:: console
 
-   (.venv) $ pip install lumache
+   python -m pip install --upgrade pip
+   pip install pdm
+
+Inside the local project directory, run:
+
+.. code-block:: console
+
+   pdm install
+
+Finally activate the virtual environment using:
+
+.. code-block:: console
+
+   eval $(pdm venv activate)
+
+That's it! You can fully run all the project in the venv.
+
+To deactivate the venv, simply run:
+
+.. code-block:: console
+
+   pdm deactivate
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+master_doc = "index"
 project = "energy_effects_t2k"
 copyright = "2024, Pierre Boistier"
 author = "Pierre Boistier"
-release = "0.1.1"
+release = "0.1.2"
 
 # -- Path setup --------------------------------------------------------------
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -30,7 +31,6 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.duration",
     "sphinx.ext.autosummary",
-    "sphinx.ext.mathjax",
     "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosectionlabel",
@@ -38,8 +38,16 @@ extensions = [
     "sphinx.ext.viewcode",
     "nbsphinx",
     "sphinx.ext.mathjax",
+    "myst_parser",
+    "sphinxcontrib.bibtex",
+    "sphinx_tabs.tabs",
 ]
 source_suffix = [".rst", ".md"]
+bibtex_bibfiles = ['refs.bib']
+
+mathjax3_config = {
+    'tex': {'tags': 'ams', 'useLabelIds': True},
+}
 
 # Add any paths that contain templates here, relative to this directory.
 
@@ -51,15 +59,29 @@ autosummary_generate = True
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_book_theme"
+# html_theme = "pydata_sphinx_theme"
+# html_title = "Energy resolution and non-linearity effects on the neutrino oscillation probability at T2K and beyond"
+html_title = "Energy effects at T2K"
+html_show_sphinx = True
+#nbsphinx_input_prompt = " "
+#nbsphinx_prompt_width = 0
+highlight_language = 'none'
+#nbsphinx_requirejs_path = ''
+
+# html_style = 'friendly'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 
 html_static_path = ["_static"]
+
+suppress_warnings = []
+
+sphinx_tabs_disable_tab_closing = True
